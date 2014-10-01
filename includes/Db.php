adbb5cd1b0fde26cef23f5ca033bb47baecd3ac6
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-01 09:13:04 +0200

    Fixed indentations

--- includes/Db.php
+++ includes/Db.php
@@ -1,29 +1,29 @@
-﻿<?php
+<?php
 
 class DatabaseConnection {
 
-	private $dbh;
+    private $dbh;
 	
-	private $host = "195.178.235.60";
-	private $database = "ac9549";
-	private $username = "ac9549";
-	private $password = "kilimanjaro911";
+    private $host = "195.178.235.60";
+    private $database = "ac9549";
+    private $username = "ac9549";
+    private $password = "kilimanjaro911";
 
-	public function __construct() {
-		$this->dbh = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=utf8', $this->username, $this->password);
-	}
+    public function __construct() {
+            $this->dbh = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=utf8', $this->username, $this->password);
+    }
 	
-	public function getAllEmployees() {
-		$stmt = $this->dbh->prepare("SELECT * FROM Anstalld");
-		$stmt->execute();
-		return $stmt->fetchAll();
-	}
-	
-	public function getEmployeeById($id) {
-		$stmt = $this->dbh->prepare("SELECT * FROM Anstalld WHERE AnstID = " . $id . "");
-		$stmt->execute();
-		return $stmt->fetch();
-	}
+    public function getAllEmployees() {
+            $stmt = $this->dbh->prepare("SELECT * FROM Anstalld");
+            $stmt->execute();
+            return $stmt->fetchAll();
+    }
+
+    public function getEmployeeById($id) {
+            $stmt = $this->dbh->prepare("SELECT * FROM Anstalld WHERE AnstID = " . $id . "");
+            $stmt->execute();
+            return $stmt->fetch();
+    }
 
 }
 	

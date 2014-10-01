1a131dfd509298480a833919e6be16612afb97f7
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-01 09:05:41 +0200

    Added database classes

---
+++ includes/Db.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+class DatabaseConnection {
+
+	private $dbh;
+	
+	private $host = "195.178.235.60";
+	private $database = "ac9549";
+	private $username = "ac9549";
+	private $password = "kilimanjaro911";
+
+	public function __construct() {
+		$this->dbh = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->database . ';charset=utf8', $this->username, $this->password);
+	}
+	
+	public function getAllEmployees() {
+		$stmt = $this->dbh->prepare("SELECT * FROM Anstalld");
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+	
+	public function getEmployeeById($id) {
+		$stmt = $this->dbh->prepare("SELECT * FROM Anstalld WHERE AnstID = " . $id . "");
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+
+}
+	
+?>
---
+++ includes/DbImpl.php
@@ -0,0 +1,47 @@
+﻿<?php
+
+	/*try {
+		$dbh = new PDO('mysql:host=195.178.235.60;dbname=ac9549;charset=utf8', 'ac9549', 'kilimanjaro911');
+		$stmt = $dbh->prepare('SELECT * FROM Anstalld');
+		$stmt->execute();
+		
+		$result = $stmt->fetchAll();
+		foreach($result as $r)
+		{
+			echo "<p>" . $r['Namn'] . "</p>";
+		}
+	}
+	catch (Exception $e)
+	{
+		echo $e->getMessage();
+	}*/
+	
+	include("dbclass.php");
+	
+	try {
+		$dbh = new DatabaseConnection();
+		
+		$result = $dbh->getAllEmployees();
+		
+		echo "<table>";
+		foreach ($result as $r) {
+			echo "<tr>";
+				echo "<td>" . $r['Namn']. "</td>";
+				echo "<td>" . $r['Adress'] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+		
+		echo "<br />";
+		echo "<br />";
+		echo "<br />";
+		
+		$id = 7;
+		$result = $dbh->getEmployeeById($id);
+		echo "Anställds namn med id " . $id . ": " . $result['Namn'] . "";
+	}
+	catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	
+?>

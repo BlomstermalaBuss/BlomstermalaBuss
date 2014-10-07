2f063405b37267d378d6df3e39b5499bcb0b527d
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-07 14:10:58 +0200

    Added basic queries

--- classes/DatabaseInterface.class.php
+++ classes/DatabaseInterface.class.php
@@ -14,31 +14,31 @@ class DatabaseInterface {
                            . 'charset=utf8', $dbConf->getUsername(), $dbConf->getPassword());
     }
 	
-    public function getEmployees() {
-        $sql = "SELECT * FROM Anstalld";
+    public function getTravels() {
+        $sql = "SELECT * FROM Travels";
         $stmt = $this->dbh->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
-
-    public function getEmployeeById($id) {
-        $sql = "SELECT * FROM Anstalld WHERE AnstID = :id";
+    
+    public function getTravelers() {
+        $sql = "SELECT * FROM Travelers";
         $stmt = $this->dbh->prepare($sql);
-        $stmt->execute(array(':id' => $id));
-        return $stmt->fetch();
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
     
-    public function getProjectsByEmployeeName($name) {
-        $sql = "SELECT Anstalld.Namn AS AnstalldNamn, Projekt.Namn AS ProjektNamn, JobbarI.TimPerV 
-                                    FROM Anstalld
-                                    INNER JOIN JobbarI
-                                    ON Anstalld.AnstID = JobbarI.AnstID
-                                    INNER JOIN Projekt 
-                                    ON JobbarI.ProjID = Projekt.ProjID
-                                    WHERE Anstalld.Namn = :name
-                                    ORDER BY Anstalld.Namn";
+    public function getWeeklySchedule() {
+        $sql = "SELECT * FROM WeeklySchedule";
         $stmt = $this->dbh->prepare($sql);
-        $stmt->execute(array(':name' => $name));
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+    
+    public function getBookings() {
+        $sql = "SELECT * FROM Booking";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute();
         return $stmt->fetchAll();
     }
 

9ff95c7d6e7052a1519c636ced31400566160c40
Emanoeel <danahb14@hotmail.com>
2014-10-09 09:50:11 +0200

    Added Cancellation page

--- pages/cancellation.php
+++ pages/cancellation.php
@@ -1,4 +1,18 @@
-<?php
+<form action="pages/processing/processing_booking.php" method="post">
+    <select size="2">
+        <option value="Trip1">Test1</option>
+        <option value="Trip2">Test2</option>
+    </select>
+    
+    <br><br>
+    <input type="text" name="Name" placeholder="Name">
+    <br><br>
+    <input type="text" name="SocialSecurityNr" placeholder="Social Security Number">
+    <br><br>
+    <input type="submit" value="Cancel your trip">
+    
+</form>
+    
 
-echo "Cnacell your reservation"
-?>
+
+  

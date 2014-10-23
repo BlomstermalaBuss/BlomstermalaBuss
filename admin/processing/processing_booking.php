0c08543bb8f055031af321e4091adce6306e56fa
Mathias Esbjörnsson <mathias.esbjornsson1@gmail.com>
2014-10-23 15:40:57 +0200

    Added dates

--- admin/processing/processing_booking.php
+++ admin/processing/processing_booking.php
@@ -1,4 +1,35 @@
-Name :  <?php echo $_POST["Name"]; ?><br>
-security:  <?php echo $_POST["SocialSecurityNr"]; ?>
+<?php  
+
+session_start();
+
+include("../../php/classes/DateGenerator.class.php");
+
+if($_POST["mode"] == "selectTravel")
+{
+    $weeklyID = $_POST["weeklyID"];
+    $travelerID = $_POST["TravelerID"];
+    
+    $all = $_SESSION["all"];
+    
+    foreach($all as $result)
+    {
+        if($result["WeeklyScheduleID"]==$weeklyID)
+        {
+            $row = $result;
+           // var_dump($row);
+        }
+    }
+    
+    $date = new DateGenerator();
+    
+    $dates = $date->calculateDates($row["Day"], 20);
+    
+    var_dump($dates);
+    
+}
 
 
+//$dbh->addBooking($travelerID, );
+
+
+?>

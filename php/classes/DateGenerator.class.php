609457d402e17ca4afd3e5d8dbdb736788167b99
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-24 00:09:28 +0200

    Renamed parameters

--- php/classes/DateGenerator.class.php
+++ php/classes/DateGenerator.class.php
@@ -5,13 +5,13 @@
  *
  * @author Daniel
  * 
- * Classes which handles operations on dates
+ * Class which handles operations on dates
  */
 class DateGenerator {
     
-    function calculateDates($day, $numberOfDays) {
+    function calculateDates($day, $numberOfDates) {
         $arr = array();
-        for($i = 0; $i < $numberOfDays; $i++) {
+        for($i = 0; $i < $numberOfDates; $i++) {
             if ($i == 0) {
                 array_push($arr, $date = date('Y-m-d', strtotime('next ' . $day)));
             } else {

b315f8694bb1a81122f1aa24a51ecc4de5a320f9
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-23 15:16:20 +0200

    Fixed weekly schedule bug

--- admin/pages/fragment/weeklyschedule_edit.php
+++ admin/pages/fragment/weeklyschedule_edit.php
@@ -1,6 +1,6 @@
 <div id="editweeklyschedule">
     <?php if(!isset($_SESSION['scheduletoedit'])) { ?>
-    <form action="php/pages/processing/processing_weeklyschedule.php" method="post">
+    <form action="processing/processing_weeklyschedule.php" method="post">
         <?php 
         $dbh = new DatabaseInterface();
         $schedules = $dbh->getWeeklySchedules();
@@ -18,17 +18,17 @@
             </tr>
         <?php
         foreach ($schedules as $schedule) {
-            echo("<tr>"
-                .  "<td>" . "<input type='radio' name='id' value='" . $schedule['WeeklyScheduleID']. "'>" . "</td>"
-                .  "<td>" . $schedule['Departure'] . "</td>"
-                .  "<td>" . $schedule['Destination'] . "</td>"
-                .  "<td>" . $schedule['Day'] . "</td>"
-                .  "<td>" . $schedule['DepartureTime'] . "</td>"
-                .  "<td>" . $schedule['ArrivalTime'] . "</td>"
-                .  "<td>" . $schedule['Price'] . "</td>"
-                . "</tr>");
-        }
-        ?>   
+            ?>
+            <tr>
+                <td><input type="radio" name="id" value="<?php echo $schedule['WeeklyScheduleID']; ?>"></td>
+                <td><?php echo $schedule['Departure']; ?></td>
+                <td><?php echo $schedule['Destination']; ?></td>
+                <td><?php echo $schedule['Day']; ?></td>
+                <td><?php echo $schedule['DepartureTime']; ?></td>
+                <td><?php echo $schedule['ArrivalTime']; ?></td>
+                <td><?php echo $schedule['Price']; ?></td>
+            </tr>
+            <?php } ?>
         </table>
         <input type="hidden" name="mode" value="editweeklyschedule">
         <p><input type="submit" value="Edit weekly schedule"></p>
@@ -36,35 +36,31 @@
     <?php } ?>
     <?php if(isset($_SESSION['scheduletoedit'])) { ?>
     <div class="formDiv">
-        <form method="POST" action="php/pages/processing/processing_weeklyschedule.php">
+        <form method="POST" action="processing/processing_weeklyschedule.php">
             <?php 
                 //var_dump($_SESSION['scheduletoedit']);
                 $result = $_SESSION['scheduletoedit'];
                 unset($_SESSION['scheduletoedit']);
-                foreach ($result as $res) {
                 ?>
-                    <p>City - departure <input type="text" name="Departure" value="<?php echo $res['Departure']; ?>"></p>
-                    <p>City - destination <input type="text" name="Destination" value="<?php echo $res['Destination']; ?>"></p>
+                    <p>City - departure <input type="text" name="Departure" value="<?php echo $result['Departure']; ?>"></p>
+                    <p>City - destination <input type="text" name="Destination" value="<?php echo $result['Destination']; ?>"></p>
                     <p>Day <select name="Day">
-                            <option value="Monday"<?php if($res['Day'] == "Monday") { echo " selected"; } ?>>Monday</option>
-                            <option value="Tuesday"<?php if($res['Day'] == "Tuesday") { echo " selected"; } ?>>Tuesday</option>
-                            <option value="Wednesday"<?php if($res['Day'] == "Wednesday") { echo " selected"; } ?>>Wednesday</option>
-                            <option value="Thursday"<?php if($res['Day'] == "Thursday") { echo " selected"; } ?>>Thursday</option>
-                            <option value="Friday"<?php if($res['Day'] == "Friday") { echo " selected"; } ?>>Friday</option>
-                            <option value="Saturday"<?php if($res['Day'] == "Saturday") { echo " selected"; } ?>>Saturday</option>
-                            <option value="Sunday"<?php if($res['Day'] == "Sunday") { echo " selected"; } ?>>Sunday</option>
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                            <option value="Saturday">Saturday</option>
+                            <option value="Sunday">Sunday</option>
                         </select>
                     </p>
-                    <p>Departure time (HH:MM) <input type="time" name="DepartureTime" value="<?php echo $res['DepartureTime']; ?>"></p>
-                    <p>Arrival time (HH:MM) <input type="time" name="ArrivalTime" value="<?php echo $res['ArrivalTime']; ?>"></p>
-                    <p>Price <input type="text" name="Price" value="<?php echo $res['Price']; ?>"></p>
-                    <p>Max traveler amount <input type="number" name="MaxTravelerAmount" value="<?php echo $res['MaxTravelerAmount']; ?>"></p>
-                    <input type="hidden" name="id" value="<?php echo $res['WeeklyScheduleID']; ?>">
+                    <p>Departure time (HH:MM) <input type="time" name="DepartureTime" value="<?php echo $result['DepartureTime']; ?>"></p>
+                    <p>Arrival time (HH:MM) <input type="time" name="ArrivalTime" value="<?php echo $result['ArrivalTime']; ?>"></p>
+                    <p>Price <input type="text" name="Price" value="<?php echo $result['Price']; ?>"></p>
+                    <p>Max traveler amount <input type="number" name="MaxTravelerAmount" value="<?php echo $result['MaxTravelerAmount']; ?>"></p>
+                    <input type="hidden" name="id" value="<?php echo $result['WeeklyScheduleID']; ?>">
                     <input type="hidden" name="mode" value="editweeklyschedule_update">
                     <p><input type="submit" value="Submit changes"></p>
-                <?php
-                } 
-            ?>
         </form>
     </div>
     <?php } ?>

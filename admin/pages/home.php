c1d3c75e8946027daa19501820b9897915dbc697
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-31 11:03:30 +0100

    Changed home text

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -1,3 +1,2 @@
-<?php
-
-    echo "hello";
+<p>Welcome to Blomstermåla Buss Booking System! To begin using the site, use the menu alternatives at the top
+of the screen.</p>
--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,2 @@
-<p>hello</p>
+<p>Welcome to Blomstermåla Buss Booking System! To begin using the site, use the menu alternatives at the top
+of the screen.</p>
